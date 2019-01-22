0a83744968cfb3339772669dd87ef6de4e877e14
maylortaylor <maylortaylor@gmail.com>
2019-01-21 23:07:12 -0500

    fix styling, need to work on about page more

--- assets/js/library.js
+++ assets/js/library.js
@@ -1,6 +1,19 @@
 (function () {
 	$(document).ready(function () {
 		$('pre').addClass('prettyprint');
+
+		function activateFilterAnimation(e) {
+			$(this).toggleClass('animated');
+		}
+
+		$('#profile-pic').on('click', activateFilterAnimation);
+		jQuery.fn.extend({
+			scrollToMe: function () {
+				var x = jQuery(this).offset().top - 100;
+				jQuery('html,body').animate({ scrollTop: x }, 400);
+			}
+		});
+		$("#about-me").scrollToMe();
 	});
 
 })();
